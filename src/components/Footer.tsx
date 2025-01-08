501124d3c7bf63b5138a36624d8c5def9b55b1cc
Miłosz Góralczyk <goralczyk.milosz2k@gmail.com>
2025-01-08 19:36:51 +0100

    footer border update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="footer fixed-bottom bg-dark text-white text-center p-3">
+    <footer className="footer fixed-bottom bg-dark text-white text-center p-3 border-top border-dark">
       <p>&copy; {new Date().getFullYear()} Flower Classifier</p>
     </footer>
   );
